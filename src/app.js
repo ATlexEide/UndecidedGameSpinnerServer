1eb31edb0a980722b0f798a04f0f89c23bd95ff7
Alexander Eide <atlexeide@gmail.com>
2025-07-24 18:07:36 +0200

    Add root route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getUserProfile } = require("./Utils/getUserProfile");
+const { getUserProfile } = require("./Utils/getUserProfile.js");
 
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => res.send("Hellooooo world"));
 app.get("/user/:id", async (req, res) => getUserProfile(req, res));
 
 app.listen(port, () => {
