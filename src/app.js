a8f178952504431e52fed2882b004c4abf15662c
Alexander Eide <atlexeide@gmail.com>
2025-07-24 18:18:58 +0200

    Fix cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const { getUserProfile } = require("./Utils/getUserProfile.js");
 
 const app = express();
+const cors = require("cors");
 const port = 3000;
+
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Hellooooo world"));
 app.get("/user/:id", async (req, res) => getUserProfile(req, res));
