2a9de9de20cae4e6ec17e032adac817cde57d250
Alexander Eide <atlexeide@gmail.com>
2025-07-24 18:54:22 +0200

    Replace old key with variable

--- src/Utils/getUserProfile.js
+++ src/Utils/getUserProfile.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 async function getUserProfile(req, res) {
   console.log(req.params.id);
   await fetch(
-    `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=44EE35206BFF58055FD20D4770B2FDB0&format=json&steamids=${req.params.id}`
+    `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${process.env.STEAM_API_KEY}&format=json&steamids=${req.params.id}`
   )
     .then((result) => result.json())
     .then((result) => res.send(result));
