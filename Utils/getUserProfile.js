28226e33b5a314698a109cf5db5a1f9212f17a39
Alexander Eide <atlexeide@gmail.com>
2025-07-24 17:37:59 +0200

    Add get user info method

---
+++ Utils/getUserProfile.js
@@ -0,0 +1,8 @@
+export async function getUserProfile(req, res) {
+  console.log(req.params.id);
+  await fetch(
+    `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=44EE35206BFF58055FD20D4770B2FDB0&format=json&steamids=${req.params.id}`
+  )
+    .then((result) => result.json())
+    .then((result) => res.send(result));
+}
