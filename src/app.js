718160661f67d9b4234f68f778dccefd5a59b330
Alexander Eide <atlexeide@gmail.com>
2025-07-28 17:26:57 +0200

    Add game details endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { getUserProfile } = require("./Utils/getUserProfile.js");
 const { getUserLibrary } = require("./Utils/getUserLibrary.js");
+const { getGameDetails } = require("./Utils/getGameDetails.js");
 
 const app = express();
 const cors = require("cors");
@@ -11,6 +12,7 @@ app.use(cors());
 app.get("/", (req, res) => res.send("Hellooooo world"));
 app.get("/user/:id", async (req, res) => getUserProfile(req, res));
 app.get("/library/:id", async (req, res) => getUserLibrary(req, res));
+app.get("/game/:id", async (req, res) => getGameDetails(req, res));
 
 app.listen(port, () => {
   console.log(`UndecidedGameSpinner server listening on port ${port}`);
