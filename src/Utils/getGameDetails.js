84e4a158270615b72be817aee0a0a98a04c68374
Alexander Eide <atlexeide@gmail.com>
2025-07-29 15:29:14 +0200

    .

--- src/Utils/getGameDetails.js
+++ src/Utils/getGameDetails.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 async function getGameDetails(req, res) {
   console.log(req.params.id);
   await fetch(
-    `https://store.steampowered.com/api/appdetails?appids=${req.params.id}`
+    `https://store.steampowered.com/api/appdetails?appids=${Number(
+      req.params.id
+    )}`
   )
     .then((result) => result.json())
     .then((result) => res.send(result));
