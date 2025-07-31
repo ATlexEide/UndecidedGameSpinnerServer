66ea785167baa29a56ee680a75da1e3d624bf71a
Alexander Eide <atlexeide@gmail.com>
2025-07-31 15:19:22 +0200

    Add error catch

--- src/Utils/getUserLibrary.js
+++ src/Utils/getUserLibrary.js
@@ -6,7 +6,11 @@ async function getUserLibrary(req, res) {
     `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${req.params.id}&format=json`
   )
     .then((result) => result.json())
-    .then((result) => res.send(result));
+    .then((result) => res.send(result))
+    .catch((err) => {
+      throw new Error(`Failed to fetch user library:
+                      ${err}`);
+    });
 }
 
 exports.getUserLibrary = getUserLibrary;
