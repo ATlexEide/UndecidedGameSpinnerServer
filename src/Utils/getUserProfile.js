adb6bc428fd21fc7a1ea235d21d5221178003bd3
Alexander Eide <atlexeide@gmail.com>
2025-07-31 15:19:25 +0200

    Add error catch

--- src/Utils/getUserProfile.js
+++ src/Utils/getUserProfile.js
@@ -6,7 +6,11 @@ async function getUserProfile(req, res) {
     `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${process.env.STEAM_API_KEY}&format=json&steamids=${req.params.id}`
   )
     .then((result) => result.json())
-    .then((result) => res.send(result));
+    .then((result) => res.send(result))
+    .catch((err) => {
+      throw new Error(`Failed to fetch user profile:
+                      ${err}`);
+    });
 }
 
 exports.getUserProfile = getUserProfile;
