1013afbf19bf8f8e919f7224955413fb5ee20cf5
Alexander Eide <atlexeide@gmail.com>
2025-08-16 22:37:33 +0200

    Update parameter

--- src/Utils/getUserLibrary.js
+++ src/Utils/getUserLibrary.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
 
-async function getUserLibrary(req) {
-  console.log(req.params.id);
+async function getUserLibrary(steamid) {
   return await fetch(
-    `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${req.params.id}&format=json`
+    `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${steamid}&format=json`
   )
     .then((result) => result.json())
     .catch((err) => {
