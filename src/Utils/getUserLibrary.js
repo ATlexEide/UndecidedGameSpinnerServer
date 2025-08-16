cca15012fd34b7435e4853fe6dd22bf5f440b444
Alexander Eide <atlexeide@gmail.com>
2025-08-16 22:25:47 +0200

    Add return

--- src/Utils/getUserLibrary.js
+++ src/Utils/getUserLibrary.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 
-async function getUserLibrary(req, res) {
+async function getUserLibrary(req) {
   console.log(req.params.id);
-  await fetch(
+  return await fetch(
     `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${req.params.id}&format=json`
   )
     .then((result) => result.json())
-    .then((result) => res.send(result))
     .catch((err) => {
       throw new Error(`Failed to fetch user library:
                       ${err}`);
--- src/Utils/getUserProfile.js
+++ src/Utils/getUserProfile.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 
-async function getUserProfile(req, res) {
-  console.log(req.params.id);
-  await fetch(
-    `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${process.env.STEAM_API_KEY}&format=json&steamids=${req.params.id}`
+async function getUserProfile(req) {
+  console.log(req);
+  console.log(req.params);
+  return await fetch(
+    `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${process.env.STEAM_API_KEY}&format=json&steamids=${req.params.id}`
   )
     .then((result) => result.json())
-    .then((result) => res.send(result))
     .catch((err) => {
       throw new Error(`Failed to fetch user profile:
                       ${err}`);
