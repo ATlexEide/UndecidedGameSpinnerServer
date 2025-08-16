388688680cd3eeb2d4ea640b68f84b78ec094ad9
Alexander Eide <atlexeide@gmail.com>
2025-08-16 22:36:46 +0200

    Add game handler errors

--- src/Errors.js
+++ src/Errors.js
@@ -4,13 +4,33 @@ const errors = {
     status: 422,
     message: "Invalid parameter(s)"
   },
+
   userHandlerNoId: {
     code: 2,
     status: 422,
     message: "Missing id in request"
   },
+
   userHandlerNoAction: {
     code: 3,
+    status: 422,
+    message: "Missing action in request"
+  },
+
+  gameHandlerDefaultError: {
+    code: 4,
+    status: 422,
+    message: "Invalid parameter(s)"
+  },
+
+  gameHandlerNoAppid: {
+    code: 5,
+    status: 422,
+    message: "Missing appid in request"
+  },
+
+  gameHandlerNoAction: {
+    code: 6,
     status: 422,
     message: "Missing action in request"
   }
